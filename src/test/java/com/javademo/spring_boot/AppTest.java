7e4acf645ac2c2e6b8dd373cbcfd39852a44e567
AnhTu <3artemis98@gmail.com>
2023-08-26 04:35:52 +0700

    first commit

---
+++ src/test/java/com/javademo/spring_boot/AppTest.java
@@ -0,0 +1,20 @@
+package com.javademo.spring_boot;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
